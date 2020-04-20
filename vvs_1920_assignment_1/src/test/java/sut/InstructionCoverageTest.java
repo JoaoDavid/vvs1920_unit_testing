5c236537bfaa2fefb9f49f77a432736422f23272
João David <joaodavid512@gmail.com>
2020-04-20 19:30:20 +0100

    fixed assertions import

--- vvs_1920_assignment_1/src/test/java/sut/InstructionCoverageTest.java
+++ vvs_1920_assignment_1/src/test/java/sut/InstructionCoverageTest.java
@@ -1,7 +1,7 @@
 package sut;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
