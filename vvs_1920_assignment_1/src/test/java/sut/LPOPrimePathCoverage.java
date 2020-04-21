46d570b9a2f9bdb0c86b24d72608bc6214eacce8
CHKLE <ye.yang98@hotmail.com>
2020-04-21 13:16:38 +0100

    Finished prime path coverage.

--- vvs_1920_assignment_1/src/test/java/sut/LPOPrimePathCoverage.java
+++ vvs_1920_assignment_1/src/test/java/sut/LPOPrimePathCoverage.java
@@ -37,7 +37,7 @@ public class LPOPrimePathCoverage {
 		assertEquals("", tst.longestPrefixOf(query));
 	}
 	
-	/*	[1,3,5,6,8,10], [6,8,10,6], [8,10,6,8], [8,10,6,7], [10,6,8,10]*/
+	/*[1,3,5,6,8,10,6,8,10,6,7]*/
 	@Test
 	public void lpoPrimePathCoverage4() {
 		String query = "a";
@@ -47,7 +47,7 @@ public class LPOPrimePathCoverage {
 		assertEquals("", tst.longestPrefixOf(query));
 	}
 	
-	/*	[1,3,5,6,8,9,11], [6,8,9,11,6], [8,9,11,6,7], [8,9,11,6,8], [9,11,6,8,9], [11,6,8,9,11]*/
+	/*[1,3,5,6,8,9,11,6,8,9,11,6,7]*/
 	@Test
 	public void lpoPrimePathCoverage5() {
 		String query = "e";
@@ -57,12 +57,148 @@ public class LPOPrimePathCoverage {
 		assertEquals("", tst.longestPrefixOf(query));
 	}
 	
+	/*[1,3,5,6,8,9,11,6,8,10,6,7]*/
 	@Test
 	public void lpoPrimePathCoverage6() {
-		String query = "e";
+		String query = "c";
 		Integer val = 0;
-		tst.put("c", val);
-		tst.put("d", val);
+		tst.put("b", val); //c > b
+		tst.put("d", val); //c < d
 		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,10,6,8,9,11,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage7() {
+		String query = "c";
+		Integer val = 0;
+		tst.put("d", val); //c < d
+		tst.put("b", val); //c > b
+		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,11,6,8,9,12,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage8() {
+		String query = "c";
+		Integer val = null;
+		tst.put("b", val); //c < b
+		tst.put("c", val); //c == c
+		tst.longestPrefixOf(query);
+		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,14,6,8,9,12,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage9() {
+		String query = "ca";
+		Integer val = null;
+		tst.put("c", val); //c == c
+		tst.put("ca", val); //a == a
+		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,14,6,8,9,11,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage10() {
+		String query = "cb";
+		Integer val = null;
+		tst.put("c", val); //c == c
+		tst.put("ca", val); //b > a
+		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,10,6,8,9,12,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage11() {
+		String query = "c";
+		Integer val = null;
+		tst.put("d", val); //c < d
+		tst.put("c", val); //c == d
+		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,14,6,8,10,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage12() {
+		String query = "ca";
+		Integer val = null;
+		tst.put("c", val); //c == c
+		tst.put("cb", val); //a < b
+		assertEquals("", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,11,6,8,9,12,13,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage13() {
+		String query = "c";
+		Integer val = 1;
+		tst.put("b", val); //c > b
+		tst.put("c", val); //c == c
+		assertEquals("c", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,13,14,6,8,9,12,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage14() {
+		String query = "ca";
+		Integer val1 = 1; //pass through 13
+		Integer val2 = null; //skip 13
+		tst.put("c", val1); //c == c
+		//if no value is pressent, ignores
+		tst.put("ca", val2); //a == a
+		assertEquals("c", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,14,6,8,9,12,13,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage15() {
+		String query = "ca";
+		Integer val1 = null; //skip 13
+		Integer val2 = 1; //pass through 13
+		tst.put("c", val1); //c == c
+		tst.put("ca", val2); //a == a
+		assertEquals("ca", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,13,14,6,8,9,12,13,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage16() {
+		String query = "ca";
+		Integer val1 = 1; //pass through 13
+		tst.put("c", val1); //c == c
+		tst.put("ca", val1); //a == a
+		assertEquals("ca", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,13,14,6,8,9,11,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage17() {
+		String query = "cb";
+		Integer val1 = 1; //pass through 13
+		tst.put("c", val1); //c == c
+		tst.put("ca", val1); //b > a
+		assertEquals("c", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,10,6,8,9,12,13,14,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage18() {
+		String query = "c";
+		Integer val1 = 1; //pass through 13
+		tst.put("d", val1); //c < d
+		tst.put("c", val1); //c == c
+		assertEquals("c", tst.longestPrefixOf(query));
+	}
+	
+	/*[1,3,5,6,8,9,12,13,14,6,8,10,6,7]*/
+	@Test
+	public void lpoPrimePathCoverage19() {
+		String query = "ca";
+		Integer val1 = 1; //pass through 13
+		tst.put("c", val1); //c == c
+		tst.put("cb", val1); //a < b
+		tst.longestPrefixOf(query);
+		assertEquals("c", tst.longestPrefixOf(query));
 	}
 }
