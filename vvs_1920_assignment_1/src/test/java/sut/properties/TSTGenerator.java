a5a2c38f48314a17756f380a01de6b9ddc0d6e34
João David <joaodavid512@gmail.com>
2020-05-09 15:27:34 +0100

    removed unnecessary method and variables

--- vvs_1920_assignment_1/src/test/java/sut/properties/TSTGenerator.java
+++ vvs_1920_assignment_1/src/test/java/sut/properties/TSTGenerator.java
@@ -10,16 +10,13 @@ import sut.TST;
  * @author João David n49448, Ye Yang n49521
  * 
  * TST Generator
- * Number of elements inside the structure may vary between minPuts and maxPuts
+ * Number of elements inside the structure may vary between MINPUTS and MAXPUTS
  * 
  */
 public class TSTGenerator extends Generator<TST<Integer>>{
 	
-	public static final String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	private static int min = 1;
-	private static int max = 20;
-	private int minPuts = 0;
-	private int maxPuts = 1000;
+	private static final int MINPUTS = 0;
+	private static final int MAXPUTS = 1000;
 	
 	public TSTGenerator(Class<TST<Integer>> type) {
 		super(type);
@@ -27,23 +24,15 @@ public class TSTGenerator extends Generator<TST<Integer>>{
 
 	@Override
 	public TST<Integer> generate(SourceOfRandomness random, GenerationStatus status) {
-		int numPuts = random.nextInt(minPuts,maxPuts);
+		MyStringGenerator myStrGen = new MyStringGenerator();
+		int numPuts = random.nextInt(MINPUTS,MAXPUTS);
 		TST<Integer> tst = new TST<>();
 		for(int i = 0; i < numPuts; i++) {
 			Integer val = random.nextInt(5000);
-			String key = generateString(random);
+			String key = myStrGen.generate(random, status);
 			tst.put(key, val);
 		}
 		return tst;
-	}
+	}	
 	
-	private String generateString(SourceOfRandomness random) {
-		int length = random.nextInt(min,max);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < length; i++) {
-			int j = random.nextInt(chars.length());
-			sb.append(chars.charAt(j));
-		}
-		return sb.toString();
-	}
 }
